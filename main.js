11610b75bd82d279666989c7207c8130c9e85b9e
Steve Olson <steveoscaro@gmail.com>
2016-08-11 15:04:49 -0600

    add clickable grid of letters

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+
+import {
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+var BoardView = require('./boardview.js')
+
+var Main = React.createClass({
+  render() {
+    return <View style={styles.container}>
+            <BoardView/>
+           </View>
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#644B62',
+  },
+});
+
+module.exports = Main;
